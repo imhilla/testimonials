21e0a389a85eea27f9e7bfdbbd6996dec31e51ac
imhilla <hillaryodhiambo282@gmail.com>
2021-02-26 16:25:28 +0300

    complete routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from 'react';
-import Home from './Components/Home'
-// import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+import Home from './Components/Home';
+import Pricing from './Components/Pricing';
+import About from './Components/About';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <div>
+          {/* <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/pricing">Pricing</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav> */}
+
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/pricing">
+              <Pricing />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
