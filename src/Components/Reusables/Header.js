e60011fc915c90eabc52e41a39cecd67bd3dbe72
imhilla <hillaryodhiambo282@gmail.com>
2021-02-26 18:18:21 +0300

    add logo

--- src/Components/Reusables/Header.js
+++ src/Components/Reusables/Header.js
@@ -3,19 +3,17 @@ import {
   Link
 } from "react-router-dom";
 import styles from '../Reusables/Header.module.css'
+import logo from '../../Images/logo.png'
 
 export default function Header() {
   return (
     <div>
       <nav className={styles.headerContainer}>
-        <div className={styles.logo}>
-          {/* <img src={ } /> */}
-        TESTIMONIALS
-        </div>
+        <img className={styles.logo} src={logo} alt="logo" />
         <ul className={styles.link}>
-            <Link className={styles.linkli} to="/">Home</Link>
-            <Link className={styles.linkli} to="/pricing">Pricing</Link>
-            <Link className={styles.linkli} to="/about">About</Link>
+          <Link className={styles.linkli} to="/">Home</Link>
+          <Link className={styles.linkli} to="/pricing">Pricing</Link>
+          <Link className={styles.linkli} to="/about">About</Link>
         </ul>
 
         <button className={styles.buttonHeader}>
