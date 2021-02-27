7179f54d72b28a30838f960bcd5d7098d47480bd
imhilla <hillaryodhiambo282@gmail.com>
2021-02-27 11:20:55 +0300

    add integration section

--- src/Components/HomeComponents/Intergaration.js
+++ src/Components/HomeComponents/Intergaration.js
@@ -3,7 +3,15 @@ import React from 'react'
 export default function Intergaration() {
   return (
     <div>
-      Intergaration section
+      <div></div>
+      <div>
+        <h1>
+          Get Clients to Give You Elaborate Testimonials
+        </h1>
+        <p>
+          Testimonials is a simple platform that allows others to make your brand trustworthy and believable
+        </p>
+      </div>
     </div>
   )
 }
