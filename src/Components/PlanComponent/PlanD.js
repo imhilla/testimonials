4903ccf3a88fbbcacf1438f689752144f30148d4
imhilla <hillaryodhiambo282@gmail.com>
2021-03-02 12:47:56 +0300

    add new tick logo

--- src/Components/PlanComponent/PlanD.js
+++ src/Components/PlanComponent/PlanD.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import styles from '../PlanComponent/PlanD.module.css'
+import styles from '../PlanComponent/PlanD.module.css';
+import tick from '../../Images/tick.png';
 
 export default function PlanD(props) {
   // const logo1 = props.logo;
-  const logo2 = props.logo2;
   const title = props.title;
   const description = props.description ? (<div>{props.description.map((data) => (
-    <div>{data}</div>
+    <div><img className={styles.tick} src={tick} alt="tick" />{data}</div>
   ))}</div>) : (<div />)
 
   return (
@@ -14,7 +14,6 @@ export default function PlanD(props) {
       {/* <h1>{logo1}</h1> */}
       <h2>{title}</h2>
       <div>
-        <div>{logo2}</div>
         {description}
       </div>
     </div>
